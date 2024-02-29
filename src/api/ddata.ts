f11131c49a2b951df83027f833e539bf175630f7
Смирнов Станислав Владимирович <ssmirnov@i-novus.ru>
2024-02-29 02:52:43 +0100

    GRP-1044: userdata get api

--- src/api/ddata.ts
+++ src/api/ddata.ts
@@ -1,5 +1,10 @@
 import express, {NextFunction, Request, Response} from "express";
 const Users = require("../models/users");
+const Organizations = require("../models/organizatons");
+const Subscriptions = require("../models/subscriptions");
+const Themes = require("../models/themes");
+const Sources = require("../models/sources");
+const Post_types = require("../models/post_types");
 const middlewares = require("../middlewares");
 import {get} from 'lodash'
 
@@ -11,20 +16,40 @@ interface SessionRequest extends Request {
 
 router.get("/userdata", middlewares.requireAuth, async (req: Request, res: Response) => {
 
-  console.log('DDATA-API-0->', req.query, req.params);
+  let responseData = {
+    user: null,
+    organization: null,
+    subscription: null,
+    themes: null
+  };
 
-  const themes = await Users.findOne({ where: { id: req.query.user_id } }).catch(
+  const user = await Users.findOne({
+    where: { id: req.query.user_id }
+  }).catch(
     (err: any) => {
       console.log("Error: ", err);
     }
   );
 
-  if (!themes)
+  if (!user)
     return res
       .status(400)
       .json({ message: "Themes not found", success: false });
-    
-  res.json({success: true, data: themes})
+
+  if(user) {
+    responseData.user = user;
+    responseData.organization = await Organizations.findOne({where: {id: user.org}});
+    responseData.subscription = await Subscriptions.findOne({where: {id: user.subscription_type}});
+    let themes = await Themes.findAll({where: {name: user.themes}});
+    if(themes && themes.length) {
+      for (let theme of themes) {
+        theme.post_types = await Post_types.findOne({where: {id: theme.type}});
+        theme.sources = await Sources.findAll({where: {id: theme.source}});
+      }
+      responseData.themes = themes;
+    }
+    res.json({success: true, data: responseData})
+  }
 });
 
 module.exports = router;
--- src/api/register.ts
+++ src/api/register.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from "express";
 const Users = require("../models/users");
 const md5 = require("md5");
-
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/register", async (req: Request, res: Response) => {
@@ -15,7 +15,7 @@ router.post("/register", async (req: Request, res: Response) => {
   );
 
   if (alreadyExistsUser) {
-    return res.status(409).json({ message: "User with email already exists!" });
+    return res.json({success: false, message: 'Пользователь с таким email уже существует'})
   }
 
   const newUser = new Users({
@@ -25,15 +25,21 @@ router.post("/register", async (req: Request, res: Response) => {
     subscription_type: 1,
     subscription_date: new Date(),
     settings: {vidgets: 'default'},
-    themes: {favorites: ['dollar']},
+    themes: ['news'],
   });
 
   const savedUser = await newUser.save().catch((err: Error) => {
     console.log("Error: ", err.message);
-    res.status(500).json({ error: `Cannot register user at the moment! ${err.message}` });
+    res.status(500).json({ success: false, message: `Cannot register user at the moment! ${err.message}` });
   });
    
-  if (savedUser) res.redirect('/login_ssrui');
+  if (savedUser) {
+    const jwtToken = jwt.sign(
+      { id: savedUser.id, email: savedUser.email },
+      process.env.JWT_SECRET
+    );
+    res.json({success: true, message: `Добро пожаловать ${savedUser.name}`, token: jwtToken, user: savedUser})
+  }
 });
 
 module.exports = router;
